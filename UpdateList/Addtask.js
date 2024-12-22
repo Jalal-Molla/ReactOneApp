45d07c5f7a535e354d341677a2aa2dcd3f1a9871
Md Jalal Molla <98682903+Jalal-Molla@users.noreply.github.com>
2024-12-22 11:42:42 +0600

    Create Addtask.js

---
+++ UpdateList/Addtask.js
@@ -0,0 +1,26 @@
+
+import { useState } from 'react';
+
+export default function AddTask({ onAddTask }) {
+  const [text, setText] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!text.trim()) return;
+    onAddTask(text);
+    setText('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit} style={{ marginBottom: '20px' }}
+    >
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Add a Bus............................."
+      />
+      <button type="submit">Add</button>
+    </form>
+  );
+}
